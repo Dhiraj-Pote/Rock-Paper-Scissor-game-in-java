c8b20bef40d88fc8432d594ec361bec3a7dc77ea
Coder/Dhiraj <91266904+Dhiraj-Pote@users.noreply.github.com>
2022-08-04 12:13:58 +0530

    Update Rock_Paper_scissor.java

--- Rock_Paper_scissor.java
+++ Rock_Paper_scissor.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Rock_Paper_scissor {
     public static void main(String[] args) {
 
-        System.out.println("Rock Paper Scissor Game...");
+        System.out.println("Rock Paper Scissor Game...");      //welcome screen
         System.out.println("_________________");
-        System.out.println("Enter 1 for Rock \uD83E\uDEA8");
+        System.out.println("Enter 1 for Rock \uD83E\uDEA8");   //instructions
         System.out.println("Enter 2 for Paper \uD83E\uDDFB");
         System.out.println("Enter 3 for Scissor ✂");
         System.out.println("_________________");
 
-        Scanner inp = new Scanner(System.in);
-        int userInput = inp.nextInt();
+        Scanner inp = new Scanner(System.in);            
+        int userInput = inp.nextInt();                  // taking user input
 
-        String uI = "";
-        switch (userInput) {
+        String uI = ""; 
+        switch (userInput) {                           // showing user input
             case 1 -> uI = "Rock \uD83E\uDEA8";
             case 2 -> uI = "Paper \uD83E\uDDFB";
             case 3 -> uI = "Scissor ✂";
@@ -25,23 +25,21 @@ public class Rock_Paper_scissor {
                 System.exit(0);
             }
         }
-
         System.out.print("User's Choice: " + uI);
 
         Random random = new Random();
-        int computerInput = random.nextInt(3) + 1;
+        int computerInput = random.nextInt(3) + 1;             // taking any random number: 1 / 2 / 3 
 
         String cI  = "";
-        switch (computerInput) {
+        switch (computerInput) {                              // showing random number
             case 1 -> cI = "Rock \uD83E\uDEA8";
             case 2 -> cI = "Paper \uD83E\uDDFB";
             case 3 -> cI = "Scissor ✂";
         }
-
         System.out.println("  |  Computer's Choice: " + cI);
         System.out.println();
 
-        if (userInput == computerInput)
+        if (userInput == computerInput)                     // Gaming Condition
             System.out.println("        Game is Draw.....");
         else if (userInput == 1 && computerInput == 3 || userInput == 2 && computerInput == 1 || userInput == 3 && computerInput == 2)
             System.out.println("          Hurray ... You Won");
